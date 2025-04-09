523d44b9f8f1a00aadaeb4980de8336d74fdccfb
AeroTide <203703008+AeroTide@users.noreply.github.com>
2025-04-09 12:52:30 -0400

    exercises complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    //check negative, numbers, and integers: a,b <0, typeof !==, Number.isInteger()
+    if (a < 0 || b < 0) return "ERROR"//negative
+    if (typeof a !== "number" || typeof b !== "number") return "ERROR"//non-number 
+    if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR"//non-integer
+    //need define high and low values
+    const min = Math.min(a, b);//don't know which is larger within a range
+    const max = Math.max(a, b);
+    //loop for summing the variables
+    let sum = 0;
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let a = Math.round((f-32)*(5/9)*10)/10;
+  return a;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let b = Math.round((c*(9/5)+32)*10)/10;
+  return b;
 };
 
 // Do not edit below this line
